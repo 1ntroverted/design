2dcb94c327444fecd40b7ac7cc18efa80f3ba94f
h_nol <developer09tark@gmail.com>
2025-11-26 23:43:14 +0900

    flashBattery

---
+++ Assets/LightManager.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using UnityEngine.UI;
+public class LightManager : MonoBehaviour
+{
+
+    [SerializeField] private float battery;
+    [SerializeField] GameObject light;
+    [SerializeField] Slider slider;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        battery = 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        battery -= (Time.deltaTime*(1/60f));
+        slider.value = battery;
+
+        if (battery <= 0f)
+        {
+            battery = 0f;
+            light.SetActive(false);
+        }
+        else
+        {
+            light.SetActive(true);
+        }
+
+    }
+}
